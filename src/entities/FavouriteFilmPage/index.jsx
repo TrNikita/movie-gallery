c636f2082c05cbded10ce827bdc0c0139e0dfc67
Nikita <tr_nikita@mail.ru>
2022-08-19 00:03:24 +0700

    init

--- src/entities/FavouriteFilmPage/index.jsx
+++ src/entities/FavouriteFilmPage/index.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {fromStorage} from '../../utils/fromStorage';
+import {ALL_FILMS, FAVOURITE_FILMS} from '../../constants/constants';
+import FilmCard from '../../common/FilmCard';
+import {filmsMock} from '../../constants/filmsMock';
 
-const FavouriteFilms = () => <div> FavouriteFilms </div>;
+const FavouriteFilms = () => {
+
+    const [allFilmsList, setAllFilmsList] = useState(fromStorage(ALL_FILMS) || filmsMock);
+    const [favouriteFilmsList, setFavouriteFilmsList] = useState(fromStorage(FAVOURITE_FILMS || []));
+
+
+    return favouriteFilmsList.length ? (
+            favouriteFilmsList.map((film) => (<React.Fragment key={film.id}>
+                <FilmCard
+                    film={film}
+                    setAllFilmsList={setAllFilmsList}
+                    setFavouriteFilmsList={setFavouriteFilmsList}
+                />
+            </React.Fragment>))
+        )
+        : (<div>List is empty</div>);
+
+};
 
 export default FavouriteFilms;
